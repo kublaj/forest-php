dede9fdbd66f9fa3a7aa8e78025e8510f377fe38
jean-marc <jean-marc@lg21.be>
2016-06-15 14:20:50 +0200

    building DoctrineAnalyzer with connection made through a PDO DSN (sort of)

--- src/ForestAdmin/Liana/Analyzer/DoctrineAnalyzer.php
+++ src/ForestAdmin/Liana/Analyzer/DoctrineAnalyzer.php
@@ -2,8 +2,144 @@
 
 namespace ForestAdmin\Liana\Analyzer;
 
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DriverManager;
 
 class DoctrineAnalyzer
 {
-    
+    protected $connection;
+
+    /**
+     * @var string
+     */
+    protected $database_url;
+
+    /**
+     * @var string
+     */
+    protected $database_name;
+
+    /**
+     * DoctrineAnalyzer constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @return array
+     */
+    public function analyze()
+    {
+        $this->initConnection();
+
+        return array(
+            'data' => $this->getData(),
+            'meta' => $this->getMeta(),
+        );
+    }
+
+    /**
+     * @param string $dbName
+     * @return $this
+     */
+    public function setDatabaseName($dbName)
+    {
+        $this->database_name = $dbName;
+        
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDatabaseName()
+    {
+        return $this->database_name;
+    }
+
+    /**
+     * @param string $dbUrl
+     * @return $this
+     */
+    public function setDatabaseUrl($dbUrl)
+    {
+        $this->database_url = $dbUrl;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDatabaseUrl()
+    {
+        return $this->database_url;
+    }
+
+    /**
+     * @param Connection $connection
+     * @return $this
+     */
+    public function setConnection(Connection $connection)
+    {
+        $this->connection = $connection;
+        
+        return $this;
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+
+    protected function initConnection()
+    {
+        $url = $this->getDatabaseUrl();
+        list($driverName, $url) = explode('://', $url);
+        list($url, $dbName) = explode('/', $url);
+        list($userpass, $hostport) = explode('@', $url);
+        list($dbUser, $dbPassword) = explode(':', $userpass);
+        list($dbHost, $dbPort) = explode(':', $hostport);
+        switch($driverName) {
+            case 'mysql':
+            case 'sqlite':
+            case 'pgsql':
+            case 'oci':
+            case 'sqlsrv':
+                $driverName = 'pdo_'.$driverName;
+                break;
+        }
+        $params = array(
+            'driver' => $driverName,
+            'username' => $dbUser,
+            'password' => $dbPassword,
+            'host' => $dbHost,
+            'port' => $dbPort,
+            'name' => $dbName,
+        );
+
+        $this->setConnection(DriverManager::getConnection($params));
+    }
+
+    /**
+     * @return array
+     */
+    protected function getData()
+    {
+        return array();
+    }
+
+    /**
+     * @return array
+     */
+    protected function getMeta()
+    {
+        $composerConfig = json_decode(file_get_contents(dirname(__FILE__).'/../../../../composer.json'), true);
+
+        return array(
+            'liana' => array_key_exists('name', $composerConfig) ? $composerConfig['name'] : '',
+            'liana-version' => array_key_exists('version', $composerConfig) ? $composerConfig['version'] : '',
+        );
+    }
 }
--- tests/DoctrineAnalyzerTest.php
+++ tests/DoctrineAnalyzerTest.php
@@ -1,12 +1,46 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: jean-marc
- * Date: 15/06/16
- * Time: 09:25
- */
+use ForestAdmin\Liana\Analyzer\DoctrineAnalyzer as DoctrineAnalyzer;
+
 class DoctrineAnalyzerTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @var DoctrineAnalyzer
+     */
+    protected $da;
 
+    protected $map;
+
+    public function setUp()
+    {
+        $config = new \Doctrine\DBAL\Configuration;
+        $this->da = new DoctrineAnalyzer;
+        $this->da
+            ->setDatabaseUrl('mysql://root:secret@localhost:33060/drapo');
+
+        $this->map = $this->da->analyze();
+        $this->assertTrue(is_array($this->map));
+    }
+
+    public function testData()
+    {
+        $map = $this->map;
+
+        $this->assertArrayHasKey('data', $map);
+
+    }
+
+    public function testMeta()
+    {
+        $map = $this->map;
+
+        $this->assertArrayHasKey('meta', $map);
+        $this->assertArrayHasKey('liana', $map['meta']);
+        $this->assertArrayHasKey('liana-version', $map['meta']);
+    }
+
+    public function tearDown()
+    {
+
+    }
 }
