c98f4884ecb4d29a8fff42ba8116add4864063e1
jean-marc <jean-marc@lg21.be>
2016-06-30 10:53:24 +0200

    added exception: CollectionNotFound

---
+++ src/ForestAdmin/Liana/Exception/CollectionNotFound.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jean-marc
+ * Date: 30/06/16
+ * Time: 10:52
+ */
+
+namespace ForestAdmin\Liana\Exception;
+
+
+class CollectionNotFound extends \Exception
+{
+
+}
